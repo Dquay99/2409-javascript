859d5e533cf4391d8c8d356ffe4270aeaf3b77fd
Dquay <anhquocpm99@gmail.com>
2024-12-04 23:41:41 +0700

    update

--- app quản lý hs/index.js
+++ app quản lý hs/index.js
@@ -1,65 +1,98 @@
-const ListStudent = [
-    {
-        id:1,
-        name:"Anh",
-        gender:"Nam",
-        MathScore:"8",
-        EnglishScore:"6",
-        LiteratureScore:"10",
-    },
-    {
-        id:2,
-        name:"Yến",
-        gender:"Nữ",
-        MathScore:"8",
-        EnglishScore:"8",
-        LiteratureScore:"10",
-    },
-    {
-        id:3,
-        name:"Nguyệt",
-        gender:"Nữ",
-        MathScore:"8",
-        EnglishScore:"10",
-        LiteratureScore:"10",
-    },
-    {
-        id:4,
-        name:"Hà",
-        gender:"Nam",
-        MathScore:"8",
-        EnglishScore:"8",
-        LiteratureScore:"10",
-    },
-    {
-        id:5,
-        name:"Đức",
-        gender:"Nam",
-        MathScore:"8",
-        EnglishScore:"6",
-        LiteratureScore:"10",
-    }
-]
+let initData = localStorage.getItem("students") ? JSON.parse(localStorage.getItem("students")) : [];
+
+// const ListStudent = [
+//     {
+//         id: 1,
+//         name: "Anh",
+//         gender: "Nam",
+//         MathScore: "8",
+//         EnglishScore: "6",
+//         LiteratureScore: "10",
+//     },
+//     {
+//         id: 2,
+//         name: "Yến",
+//         gender: "Nữ",
+//         MathScore: "8",
+//         EnglishScore: "8",
+//         LiteratureScore: "10",
+//     },
+//     {
+//         id: 3,
+//         name: "Nguyệt",
+//         gender: "Nữ",
+//         MathScore: "8",
+//         EnglishScore: "10",
+//         LiteratureScore: "10",
+//     },
+//     {
+//         id: 4,
+//         name: "Hà",
+//         gender: "Nam",
+//         MathScore: "8",
+//         EnglishScore: "8",
+//         LiteratureScore: "10",
+//     },
+//     {
+//         id: 5,
+//         name: "Đức",
+//         gender: "Nam",
+//         MathScore: "8",
+//         EnglishScore: "6",
+//         LiteratureScore: "10",
+//     }
+// ]
 
 const tbodyValue = document.getElementById('tbodylist');
+const nameValue = document.getElementById('name').value;
+const gender = document.querySelector('input[name="Gender"]:checked').value;
+const mathScore = parseFloat(document.getElementById('math').value);
+const englishScore = parseFloat(document.getElementById('english').value);
+const literatureScore = parseFloat(document.getElementById('literature').value);
 
-(function displayList(){
-ListStudent.forEach(student => {
-    const row = document.createElement('tr');
-    row.innerHTML = `
+function displayList() {
+    tbodyValue.innerHTML = '';
+    initData.forEach((student, index) => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
                 <td>${student.id}</td>
-                <td>${student.name}</td>
+                <td>${student.nameValue}</td>
                 <td>${student.gender}</td>
-                <td>${student.MathScore}</td>
-                <td>${student.EnglishScore}</td>
-                <td>${student.LiteratureScore}</td>
-                <td>${parseInt(student.MathScore) + parseInt(student.EnglishScore) + parseInt(student.LiteratureScore)/3}</td>
+                <td>${student.mathScore}</td>
+                <td>${student.englishScore}</td>
+                <td>${student.literatureScore}</td>
+                <td>${parseInt(student.mathScore) + parseInt(student.englishScore) + parseInt(student.literatureScore) / 3}</td>
                 <td>
-                    <button>Sửa</button>
-                    <button>Xóa</button>
+                    <button onclick="editStudent(${index})">Sửa</button>
+                    <button onclick="deleteStudent(${index})">Xóa</button>
                 </td>            
     `;
-    tbodyValue.appendChild(row);
-})();
-})();
+        tbodyValue.appendChild(row);
+    });
+};
+displayList();
+
+function createStudent() {
+    const id = initData.length ? initData[initData.length - 1].id + 1 : 1;
+    const student = {
+        id, nameValue, gender, mathScore, englishScore, literatureScore,
+        averageScore: ((mathScore + englishScore + literatureScore) / 3).toFixed(2)
+
+    };
+    initData.push(student);
+    localStorage.setItem("student", JSON.stringify(initData));
+    displayList(initData);
+};
+
+function deleteStudent(index) {
+    initData.splice(index, 1);
+    localStorage.setItem("student", JSON.stringify(initData));
+    displayList();
+};
+
+function updateStudent(index) {
+
+
+
+}
 
